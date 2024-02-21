acef5cb025cf20948336206c487c5a19f953a610
Racility <dimasfakhrul2@gmail.com>
2024-02-21 17:32:16 +0700

    baru

--- resources/views/Layouts/admin.blade.php
+++ resources/views/Layouts/admin.blade.php
@@ -1,4 +1,4 @@
-li<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -5,4 +5,4 @@
     rel="stylesheet">
 
 <!-- Custom styles for this template-->
-<link href="css/sb-admin-2.min.css" rel="stylesheet">
+<link href="{{ asset('template/css/sb-admin-2.min.css') }}" rel="stylesheet">
